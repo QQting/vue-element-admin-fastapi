7a21e2e03517344b39b2f7a31cea7013aee919ea
H-HChen <annhunter201@gmail.com>
2021-04-22 15:05:50 +0800

    Move wifi test to new file and add get config test

--- backend/app/app/tests/api/api_v1/test_robot.py
+++ backend/app/app/tests/api/api_v1/test_robot.py
@@ -1,33 +1,30 @@
 from typing import Dict
 from fastapi.testclient import TestClient
-from app.api.api_v1.robots.RMT_core import rmt_py_wrapper
 from app.core.config import settings
 import os
 import pytest
 import subprocess
-import string
 import random
+import time
 
-def str_generator(size, chars=string.ascii_letters + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+@pytest.fixture(scope="module", autouse=True)
+def change_dir():
+    os.chdir("../../../api/api_v1/robots/RMT_core/")
 
-@pytest.fixture(scope="class")
-def get_agent_num():
-    rmt_py_wrapper.rmt_server_init()
-    num_ptr = rmt_py_wrapper.new_intptr()
-    dev_list = rmt_py_wrapper.device_info_list.frompointer(rmt_py_wrapper.rmt_server_create_device_list(num_ptr))
-    num = rmt_py_wrapper.intptr_value(num_ptr)
-    rmt_py_wrapper.delete_intptr(num_ptr) # release num_ptr
+@pytest.fixture(scope="module", autouse=True)
+def agent_start_id():
+    return random.randint(0,500)
 
-    return num
+@pytest.fixture(scope="module", autouse=True)
+def generate_agent(agent_start_id):
+    subprocess.run(["killall", "agent_example"])
+    num = random.randint(0,10)
+    process = subprocess.Popen(["python3", "multi_agents.py", "-n", str(num), "-s", str(agent_start_id)])
+    time.sleep(2)
+    yield num
+    process.kill()
+    subprocess.run(["killall", "agent_example"])
 
-@pytest.fixture(scope="class")
-def default_wifi_data():
-    return {"ssid": "RMTserver",
-            "password": "adlinkros",
-            "band": "2.4 GHz",
-            "mode_on": False}
-    
 class TestRobotPage:
     @pytest.fixture(scope="class", autouse=True)
     def get_robot_list(self, client: TestClient, superuser_token_headers: Dict[str, str]):
@@ -39,69 +36,48 @@ class TestRobotPage:
     def agent_data(self, get_robot_list):
         return get_robot_list.json()["data"]
 
-    def test_status_code(self, get_robot_list) -> None:
+    def test_status_code(self, get_robot_list):
         assert get_robot_list.status_code == 200
 
-    def test_robot_data(self, agent_data, get_agent_num):
-        if get_agent_num:
-            assert agent_data["total"] == get_agent_num
+    def test_robot_data(self, agent_data, generate_agent):
+        assert agent_data["total"] == generate_agent
+        if generate_agent:
             for agent in agent_data["items"]:
                 for feature in agent:
                     assert agent[feature]
 
-class TestWifiGetRequest:
+class TestGetConfig:
     @pytest.fixture(scope="class", autouse=True)
-    def get_wifi_set(self, client: TestClient, superuser_token_headers: Dict[str, str]):
-        response = client.get(
-            "/robots/wifi-init", headers=superuser_token_headers)
+    def default_config_data(self):
+        return {"custom_config_list": ["cpu","ram","hostname","wifi"]}
+    @pytest.fixture(scope="class", autouse=True)
+    def get_config_all(self, client: TestClient, superuser_token_headers: Dict[str, str], default_config_data):
+        response = client.post(
+            "/robots/get_config", headers=superuser_token_headers, json=default_config_data)
         return response
 
     @pytest.fixture(scope="class", autouse=True)
-    def user_data(self, get_wifi_set):
-        return get_wifi_set.json()["data"]
+    def agent_data(self, get_config_all):
+        return get_config_all.json()["data"]
 
-    def test_status_code(self, get_wifi_set) -> None:
-        assert get_wifi_set.status_code == 200
+    def test_status_code(self, get_config_all):
+        assert get_config_all.status_code == 200
 
-    @pytest.mark.parametrize("config_name", ["password", "ssid", "band"])
-    def test_wifi_pwd(self, user_data, config_name) -> None:
-        assert user_data[config_name]
+    def test_config_data(self, agent_data, generate_agent):
+        assert len(agent_data) == generate_agent
+        if generate_agent:
+            for agent in agent_data:
+                    for feature in agent:
+                        assert agent[feature]
 
-    def test_default_wifi(self, client: TestClient, superuser_token_headers: Dict[str, str], default_wifi_data):
-        if "RMTHost.nmconnection" in os.listdir("/etc/NetworkManager/system-connections"):
-            result = subprocess.run(["nmcli", "con", "delete", "RMTHost"], stdout=subprocess.PIPE)
-        response = client.get(
-            "/robots/wifi-init", headers=superuser_token_headers)
-        assert response.json()["data"] == default_wifi_data
+    def test_config_id(self, client: TestClient, superuser_token_headers: Dict[str, str], agent_start_id, generate_agent, default_config_data):
+        for id in range(agent_start_id, agent_start_id+generate_agent):
+            response = client.post(
+                f"/robots/get_config/{id}", headers=superuser_token_headers, json=default_config_data)
+            assert response.status_code == 200
 
-class TestWifiPostRequest:
-    @pytest.fixture(scope="class", params=[(33,100,8,32), (1,32,0,7), (1,32,33,100)], ids=["ssid_over","pwd_short","pwd_over"])
-    def faux_response(self, request, client: TestClient, superuser_token_headers: Dict[str, str], default_wifi_data):
-        faux_set = {"ssid": str_generator(size=random.randint(request.param[0], request.param[1])),
-                    "password": str_generator(size=random.randint(request.param[2], request.param[3])),
-                    "band": "2.4 GHz",
-                    "mode_on": False}
-        response = client.post("/robots/wifi", headers=superuser_token_headers, json=faux_set)
-        yield response
-        result = subprocess.run(["nmcli", "con", "delete", "RMTHost"], stdout=subprocess.PIPE)
-        response = client.post("/robots/wifi", headers=superuser_token_headers, json=default_wifi_data)
-
-    @pytest.fixture(scope="class", params=[True, False], ids=["init","exist"])
-    def post_response(self, request, client: TestClient, superuser_token_headers: Dict[str, str]):
-        if "RMTHost.nmconnection" in os.listdir("/etc/NetworkManager/system-connections") and request.param:
-            result = subprocess.run(["nmcli", "con", "delete", "RMTHost"], stdout=subprocess.PIPE)
-        test_set = {"ssid": str_generator(size=random.randint(1, 32)),
-                    "password": str_generator(size=random.randint(8, 32)),
-                    "band": "2.4 GHz",
-                    "mode_on": request.param}
-        response = client.post("/robots/wifi", headers=superuser_token_headers, json=test_set)
-        return response
-
-    def test_post_wifi(self, post_response) -> None: 
-        assert post_response.status_code == 200
-        assert "Error" not in post_response.json()["data"]
-
-    def test_post_faux(self, faux_response):
-        print(faux_response.json())
+    def test_faux_id(self, client: TestClient, superuser_token_headers: Dict[str, str], agent_start_id, default_config_data):
+        response = client.post(
+                f"/robots/get_config/{agent_start_id+20}", headers=superuser_token_headers, json=default_config_data)
         with pytest.raises(AssertionError):
-            assert faux_response.status_code == 200
+            assert response.json()["code"] == 20000
