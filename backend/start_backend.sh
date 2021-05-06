8441d657e7265126ffb3f8210fbc0b736cf0dba3
Ting Chang <ting.chang@adlinktech.com>
2021-05-06 17:43:11 +0800

    Add file_transfer API

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 WORK_DIR=$PWD
-export LD_LIBRARY_PATH=$WORK_DIR/app/app/api/api_v1/robots/RMT_core/
+#export LD_LIBRARY_PATH=$WORK_DIR/app/app/api/api_v1/robots/RMT_core/
 python $WORK_DIR/app/app/main.py
